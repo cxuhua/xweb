bc6f6f6b066a8c75845537a31bc8598456d16818
xuhua <cxuhua@gmail.com>
2016-06-18 17:40:08 +0800

    add ZeroTime get today zero time

--- util.go
+++ util.go
@@ -1,1 +1,16 @@
 package xweb
+
+import (
+	"time"
+)
+
+//获得当天0点时间
+func ZeroTime() time.Time {
+	now := time.Now()
+	fmt := now.Format("2006-01-02")
+	zv, err := time.ParseInLocation("2006-01-02", fmt, time.Local)
+	if err != nil {
+		panic(err)
+	}
+	return zv
+}
---
+++ util_test.go
@@ -0,0 +1,28 @@
+package xweb
+
+import (
+	. "gopkg.in/check.v1"
+	"time"
+)
+
+type UtilSuite struct {
+}
+
+var _ = Suite(&UtilSuite{})
+
+func (this *UtilSuite) SetUpSuite(c *C) {
+
+}
+
+func (this *UtilSuite) TearDownSuite(c *C) {
+
+}
+
+func (this *UtilSuite) TestZeroTime(c *C) {
+	v1 := ZeroTime().Unix()
+	time.Sleep(time.Second)
+	v2 := ZeroTime().Unix()
+	c.Assert(v1, Equals, v2)
+	ns := time.Now().Format("2006-01-02")
+	c.Assert(ns, Equals, ZeroTime().Format("2006-01-02"))
+}
