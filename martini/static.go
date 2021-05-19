f52ad5f297ca0738365390041d84ed2a258c0db7
xuhua <cxuhua@gmail.com>
2021-05-19 17:08:26 +0800

    s

--- martini/static.go
+++ martini/static.go
@@ -51,7 +51,7 @@ func prepareStaticOptions(options []StaticOptions) StaticOptions {
 }
 
 // StaticFS returns a middleware handler that serves static files in the given directory.
-func StaticFS(directory string,dir http.FileSystem, staticOpt ...StaticOptions) Handler {
+func StaticFS(directory string, dir http.FileSystem, staticOpt ...StaticOptions) Handler {
 	baseDir := directory
 	opt := prepareStaticOptions(staticOpt)
 	return func(res http.ResponseWriter, req *http.Request, log *logging.Logger) {
@@ -73,9 +73,8 @@ func StaticFS(directory string,dir http.FileSystem, staticOpt ...StaticOptions) 
 			}
 		}
 		if baseDir != "" {
-			file = path.Join(baseDir,file)
+			file = path.Join(baseDir, file)
 		}
-		log.Println("----------",baseDir, file)
 		f, err := dir.Open(file)
 		if err != nil {
 			// try any fallback before giving up
@@ -142,5 +141,5 @@ func Static(directory string, staticOpt ...StaticOptions) Handler {
 	if !filepath.IsAbs(directory) {
 		directory = filepath.Join(Root, directory)
 	}
-	return StaticFS("",http.Dir(directory), staticOpt...)
+	return StaticFS("", http.Dir(directory), staticOpt...)
 }
